69b4107c4f34537d3a7b62a79bfbeeb477ab74a9
Jeremy T <jeremytupas@gmail.com>
2022-04-26 09:20:41 -0500

    Setup testing environment

---
+++ ship.js
@@ -0,0 +1,5 @@
+const foo = (a, b) => {
+    return a + b;
+};
+
+export { foo };
---
+++ ship.test.js
@@ -0,0 +1,5 @@
+import { foo } from './ship.js';
+
+test('adds two numbers', () => {
+    expect(foo(1,2)).toBe(3);
+});
