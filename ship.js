55578f1de4a8d58cc4d595b0a32e75a25193fb95
Jeremy T <jeremytupas@gmail.com>
2022-04-26 21:50:47 -0500

    Partially wrote main functions

--- ship.js
+++ ship.js
@@ -2,4 +2,51 @@ const foo = (a, b) => {
     return a + b;
 };
 
-export { foo };
+//Initialize Players and Gameboards
+//Each gameboard should be populated with predetermined/hardcoded coordinates for now. Ship placement later.
+//HTML should display both player boards and render them using Gameboard object
+    //Need gameboard render method, take user attack input method in the form of a click.
+//Call end game method from Game module when game ends.
+
+const Ship = (length, hit, sunk) => {
+    const markHit = (x, y) => {
+        //Mark passed in coordinates as 'hit', sets hit to True
+        //Mark passed in number (length / index) and mark as 'hit'.
+        const coordsHit = {
+            coordinates: [x, y],
+            coordHit: true,
+        };
+        return coordsHit;
+    };
+    const determineSunk = () => {
+        //If all positions are hit, sunk becomes True. Loop through length, checking if all are hit.
+        //Maybe receive some sort of array of coordsHit objects, loop through it and if hit is true for all of them, then sunk is true
+    };
+    return { length, hit, sunk, markHit, determineSunk };
+};
+
+const Gameboard = () => {
+    const placeShip = () => {
+        const newShip = Ship(4, false, false);
+        //Put ship on grid
+    };
+    const receiveAttack = (coords) => {
+        //hit or miss
+        //if hit markhit to the correct ship
+        //else if miss record coords of miss
+    };
+    const missedAttacks = [];
+    const determineAllSunk = () => {
+        //Check if all ships have been sunk
+    };
+};
+
+const Player = () => {
+    let ownTurn = false;
+    const randomPlay = () => {
+        //Make a random legal move
+    };
+    return { ownTurn, randomPlay };
+};
+
+export { foo, Ship };
--- ship.test.js
+++ ship.test.js
@@ -1,5 +1,16 @@
-import { foo } from './ship.js';
+import { foo, Ship } from './ship.js';
+
+const testShip = Ship(4, false, false);
 
 test('adds two numbers', () => {
     expect(foo(1,2)).toBe(3);
 });
+
+test('marks hit', () => {
+    //After calling the function, expect hit property to be true
+    expect(testShip.markHit(1,2)).toEqual([1,2]);
+});
+
+// test('determine if ship sank', () => {
+
+// });
